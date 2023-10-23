35157883aa7fed973c6d43636d9405d849961edf
ijunc2 <ijunc2@gmail.com>
2023-10-23 13:55:59 +0900

    updated

--- js/pcd_label_tool.js
+++ js/pcd_label_tool.js
@@ -2985,9 +2985,11 @@ function createGrid() {
     let posZLidar;
     let translationX;
     if (labelTool.currentDataset === labelTool.datasets.NuScenes) {
+        console.log("11111")
         posZLidar = labelTool.positionLidarNuscenes[2];
         translationX = 0;
     } else {
+        console.log("22222")
         posZLidar = labelTool.positionLidar[2];
         translationX = gridSize / 2;
     }
